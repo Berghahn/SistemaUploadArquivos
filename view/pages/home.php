f545371d0bf3f0ca6770d327cadaf5a421842b2f
Berghahn <72152352+Berghahn@users.noreply.github.com>
2021-05-28 15:49:22 -0300

    Add files via upload

---
+++ view/pages/home.php
@@ -0,0 +1,24 @@
+<h1 style="Text-align: center;">Inicio</h1>
+<h3>&nbsp &nbsp &nbsp Softwares padrão que devem ser instalados</h3>
+<p>
+    -Winrar.</br>
+    -Chrome.</br>
+    -Firefox.</br>
+    -Adobe Reader DC.</br>
+    -Thunderbird.</br>
+    -UltraVNC.</br>
+    -Pacote Office 2016 HOME (se tiver licença, caso não, broffice).</br>
+</p>
+<h3>&nbsp &nbsp &nbsp  Casos específicos</h3>
+<p>
+    -Adobe XI PRO.</br>
+    <h4>Projetos</h4>
+    -SOM.</br>
+    -Corel Draw.</br>
+    -Autocad.</br>
+    
+
+
+
+
+</p>
